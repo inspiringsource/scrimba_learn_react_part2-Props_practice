66d6976ef5eb133dd5aee04006da2f770bf6c009
inspiringsource <ab.b2k3@gmail.com>
2021-12-27 17:43:37 +0100

    conditional rendering

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,9 +1,15 @@
+import React from "react"
 
-export default function Jokes(props) {
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
     return (
         <div>
             {props.setup && <h3>{props.setup}</h3>}
-            <p>{props.punchline}</p>
+            {isShown && <p>{props.punchline}</p>}
+            {!isShown && <button onClick={toggleShown}>Show Punchline</button>}
             <hr />
         </div>
     )
